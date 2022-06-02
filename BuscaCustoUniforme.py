5f12cc6a0e30e931abd86b4d40864e119580ce1d
Lucas Felipe <lcky123hop@gmail.com>
2022-06-02 19:11:01 -0300

    update UCS e BrFS

--- BuscaCustoUniforme.py
+++ BuscaCustoUniforme.py
@@ -1,106 +1,120 @@
-# Retorna o menor caminho
-def ucs(vertDes, vertIni):
+# returns the minimum cost in a vector( if
+# there are multiple goal states)
+def uniform_cost_search(goal, start):
 	
-	global grafo, custo
-	custos = []
+	# minimum cost upto
+	# goal state from starting
+	global graph,cost
+	answer = []
 
-	# Cria uma fila de prioridades
-	filaPrio = []
+	# create a priority queue
+	queue = []
 
-	# Diz que todos os caminhos sao infinitos
-	for i in range(len(vertDes)):
-		custos.append(10**8)
+	# set the answer vector to max value
+	for i in range(len(goal)):
+		answer.append(10**8)
 
-	# poe na fila o primeiro vertice
-	filaPrio.append([0, vertIni])
+	# insert the starting index
+	queue.append([0, start])
 
-	# guarda os nós visitados
-	visitados = {}
-	contador = 0
+	# map to store visited node
+	visited = {}
 
-	# Enquanto a fila nao estiver vazia
-	while (len(filaPrio) > 0):
+	# count
+	count = 0
 
-		# Pega o elemento no topo da fila
-		filaPrio = sorted(filaPrio)
-		maiorEle = filaPrio[-1]
+	# while the queue is not empty
+	while (len(queue) > 0):
 
-		# remove o elemento
-		del filaPrio[-1]
+		# get the top element of the
+		queue = sorted(queue)
+		p = queue[-1]
 
-		# pega o valor original
-		maiorEle[0] *= -1
+		# pop the element
+		del queue[-1]
 
-		# verifica se o elemento faz parte do objetivo
-		if (maiorEle[1] in vertDes):
+		# get the original value
+		p[0] *= -1
 
-			# pega a posicao
-			index = vertDes.index(maiorEle[1])
+		# check if the element is part of
+		# the goal list
+		if (p[1] in goal):
 
-			# se um novo caminho for encontrado
-			# aumenta o contador
-			if (custos[index] == 10**8):
-				contador += 1
+			# get the position
+			index = goal.index(p[1])
 
-			# se o custo eh menor, garda o custo
-			if (custos[index] > maiorEle[0]):
-				custos[index] = maiorEle[0]
+			# if a new goal is reached
+			if (answer[index] == 10**8):
+				count += 1
 
-			# remove o elemento
-			del filaPrio[-1]
+			# if the cost is less
+			if (answer[index] > p[0]):
+				answer[index] = p[0]
 
-			# organiza a fila
-			filaPrio = sorted(filaPrio)
-			if (contador == len(vertDes)):
-				return custos
+			# pop the element
+			del queue[-1]
 
-		# verifica se ha nos nao visitados do no atual
-		if (maiorEle[1] not in visitados):
-			for i in range(len(grafo[maiorEle[1]])):
+			queue = sorted(queue)
+			if (count == len(goal)):
+				return answer
 
-				# valor multiplicado por -1 para ir para o topo da fila
-				filaPrio.append( [(maiorEle[0] + custo[(maiorEle[1], grafo[maiorEle[1]][i])])* -1, grafo[maiorEle[1]][i]])
+		# check for the non visited nodes
+		# which are adjacent to present node
+		if (p[1] not in visited):
+			for i in range(len(graph[p[1]])):
 
-		# seta como visitado
-		visitados[maiorEle[1]] = 1
+				# value is multiplied by -1 so that
+				# least priority is at the top
+				queue.append( [(p[0] + cost[(p[1], graph[p[1]][i])])* -1, graph[p[1]][i]])
 
-	return custos
+		# mark as visited
+		visited[p[1]] = 1
 
-# main
-if _name_ == '_main_':
+	return answer
+
+# main function
+if __name__ == '__main__':
 	
-	# cria o grafo
-	grafo,custo = [[] for i in range(8)],{}
+	# create the graph
+	graph,cost = [[] for i in range(8)],{}
 
-	grafo[0].append(1)
-	grafo[0].append(3)
-	grafo[3].append(1)
-	grafo[3].append(6)
-	grafo[3].append(4)
-	grafo[1].append(6)
-	grafo[4].append(2)
-	grafo[4].append(5)
-	grafo[2].append(1)
-	grafo[5].append(2)
-	grafo[5].append(6)
-	grafo[6].append(4)
+	# add edge
+	graph[0].append(1)
+	graph[0].append(3)
+	graph[3].append(1)
+	graph[3].append(6)
+	graph[3].append(4)
+	graph[1].append(6)
+	graph[4].append(2)
+	graph[4].append(5)
+	graph[2].append(1)
+	graph[5].append(2)
+	graph[5].append(6)
+	graph[6].append(4)
 
-	custo[(0, 1)] = 2
-	custo[(0, 3)] = 5
-	custo[(1, 6)] = 1
-	custo[(3, 1)] = 5
-	custo[(3, 6)] = 6
-	custo[(3, 4)] = 2
-	custo[(2, 1)] = 4
-	custo[(4, 2)] = 4
-	custo[(4, 5)] = 3
-	custo[(5, 2)] = 6
-	custo[(5, 6)] = 3
-	custo[(6, 4)] = 7
-	
+	# add the cost
+	cost[(0, 1)] = 2
+	cost[(0, 3)] = 5
+	cost[(1, 6)] = 1
+	cost[(3, 1)] = 5
+	cost[(3, 6)] = 6
+	cost[(3, 4)] = 2
+	cost[(2, 1)] = 4
+	cost[(4, 2)] = 4
+	cost[(4, 5)] = 3
+	cost[(5, 2)] = 6
+	cost[(5, 6)] = 3
+	cost[(6, 4)] = 7
+
+	# goal state
 	goal = []
-	goal.append(6)
-	answer = ucs(goal, 0)
 
-	# Exibe a resposta
-	print("Custo minimo de 0 ate 6 eh = ",answer[0])
+	# set the goal
+	# there can be multiple goal states
+	goal.append(6)
+
+	# get the answer
+	answer = uniform_cost_search(goal, 0)
+
+	# print the answer
+	print("O custo mínimo de 0 a 6 é =",answer[0])
--- BuscaEmLargura.py
+++ BuscaEmLargura.py
@@ -1,54 +1,28 @@
-from collections import defaultdict
- 
-# Grafico Direcionado
-class Grafo:
- 
-    # Construtor
-    def _init_(self):
-        # Dicionario para guardar o grafo
-        self.grafo = defaultdict(list)
- 
-    # function to add an edge to graph
-    def addVertice(self,vertOri,vertDes):
-        self.grafo[vertOri].append(vertDes)
- 
-    # Function to print a BFS of graph
-    def bfs(self, vertOri):
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
 
-        # Marca todos os vertices como nao visitados
-        visitados = [False] * (max(self.grafo) + 1)
-			
-        # Cria uma fila para usar no algoritmo
-        fila = []
- 
-        # Marca o vertice de origem como visitado
-				# E o coloca na lista
-        fila.append(vertOri)
-        visitados[vertOri] = True
- 
-        while fila:
-            # Retira um vertice da fila
-						# E exibe o seu valor
-            s = fila.pop(0)
-            print (s, end = " ")
- 
-            # Pega todos os vertices adjacentes do
-						# vertice atual, e caso um deles nao tenha
-						# sido visitado, o visita
-            for i in self.grafo[s]:
-                if visitados[i] == False:
-                    fila.append(i)
-                    visitados[i] = True
- 
-# Teste
-grafo = Grafo()
-grafo.addVertice(0, 1)
-grafo.addVertice(0, 2)
-grafo.addVertice(1, 2)
-grafo.addVertice(2, 0)
-grafo.addVertice(2, 3)
-grafo.addVertice(3, 3)
-grafo.addVertice(3, 1)
- 
-print ("Iniciando do vertice 2: ")
-grafo.bfs(2)
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
+
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0) 
+    print (m, end = " ") 
+
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+print("A seguir está a pesquisa em largura")
+bfs(visited, graph, '5')    # function calling
